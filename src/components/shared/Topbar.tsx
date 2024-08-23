bd5a6fc9ed28408566e52311f3e3d5e37d606168
PRIYA RANA <127029448+ranapiya@users.noreply.github.com>
2024-08-23 19:28:05 +0530

    Update Topbar.tsx

--- src/components/shared/Topbar.tsx
+++ src/components/shared/Topbar.tsx
@@ -17,14 +17,7 @@ const Topbar = () => {
   return (
     <section className="topbar">
       <div className="flex-between py-4 px-5">
-        <Link to="/" className="flex gap-3 items-center">
-          <img
-            src="/assets/images/logo.svg"
-            alt="logo"
-            width={130}
-            height={325}
-          />
-        </Link>
+       
 
         <div className="flex gap-4">
           <Button
